1f1bc1729efbb0cc31f0d547291acc595b08f650
Toung-test <C108193246@nkust.edu.tw>
2025-12-16 17:27:02 +0800

    設定好初步路由 & 建立好我預計需要的page跟component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './page/index/index.component';
+import { LoginComponent } from './page/login/login.component';
+import { AdminComponent } from './page/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent }
+];
